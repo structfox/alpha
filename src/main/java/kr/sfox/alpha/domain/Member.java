1751262e6229144dcfdb28436d69f094b79587ef
structfox <structfox@gmail.com>
2024-12-08 21:06:37 +0900

    Member  Entity에 Auditing 기능 추가
    createdAt, updatedAt 자동 입력 적용

--- src/main/java/kr/sfox/alpha/domain/Member.java
+++ src/main/java/kr/sfox/alpha/domain/Member.java
@@ -1,19 +1,18 @@
 package kr.sfox.alpha.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
+@EntityListeners(AuditingEntityListener.class) // Auditing 활성화
 public class Member {
     @Id
     @GeneratedValue
