f7273e0f5848944d2f6f4acf9470bc338f6bbe17
structfox <structfox@gmail.com>
2024-12-08 19:51:16 +0900

    Member Entity  속성 추가

--- src/main/java/kr/sfox/alpha/domain/Member.java
+++ src/main/java/kr/sfox/alpha/domain/Member.java
@@ -1,10 +1,16 @@
 package kr.sfox.alpha.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
@@ -13,6 +19,17 @@ public class Member {
     @GeneratedValue
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String userId;
+
+    @Column(nullable = false)
     private String password;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false) // 생성일은 수정되지 않도록 설정
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 }
