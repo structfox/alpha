1c22d6a47df6d63ca7faf61ca02732b2af89599b
structfox <structfox@gmail.com>
2024-12-09 23:45:14 +0900

    member 저장 시 password를 bcrypt 해싱하여 저장

--- src/main/java/kr/sfox/alpha/Service/MemberService.java
+++ src/main/java/kr/sfox/alpha/Service/MemberService.java
@@ -1,8 +1,10 @@
 package kr.sfox.alpha.Service;
 
 import kr.sfox.alpha.Repository.MemberRepository;
+import kr.sfox.alpha.api.memberDto.request.JoinMemberRequest;
 import kr.sfox.alpha.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +12,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public Long join(Member member) {
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
         memberRepository.save(member);
         return member.getId();
     }
